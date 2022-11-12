a1fcd6a464fb2ef065d0ac62aed2253ddfd759eb
FriskKing <402490889@qq.com>
2022-11-12 15:11:50 +0800

    配置更新

--- config.inc.php
+++ config.inc.php
@@ -29,12 +29,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => '数据库地址',
-  'user' => '数据库用户名',
-  'password' => '数据库密码',
+  'host' => 'sql.freedb.tech',
+  'user' => 'freedb_Fei_xiangShi',
+  'password' => 'P8vtP&6vFWQxeP2',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库昵称',
+  'database' => 'freedb_MyBlog',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
